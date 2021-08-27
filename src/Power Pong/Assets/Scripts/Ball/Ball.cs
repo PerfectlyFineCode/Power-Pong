6fc122770f67150cffc9cd7a1e7c1ffc9e0e224a
ADEL\spp109 <martin.magnusson@adelfors.nu>
2021-08-27 11:12:30 +0200

    Changed velocity system

--- src/Power Pong/Assets/Scripts/Ball/Ball.cs
+++ src/Power Pong/Assets/Scripts/Ball/Ball.cs
@@ -3,10 +3,12 @@ public class Ball : BallBase
 	// Start is called before the first frame update
 	private void Start()
 	{
+		InitializeVelocity();
 	}
 
 	private void FixedUpdate()
 	{
 		DoFixedUpdate();
+		UpdateVelocity();
 	}
 }
--- src/Power Pong/Assets/Scripts/Ball/Base/BallBase.cs
+++ src/Power Pong/Assets/Scripts/Ball/Base/BallBase.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]
@@ -7,8 +8,7 @@ public class BallBase : MonoBehaviour, IBall
 	private Rigidbody2D rb;
 	private SpriteRenderer SpriteRenderer;
 
-	[field: SerializeField]
-	private Vector2 Velocity { get; set; } = Vector2.right;
+	[field: SerializeField] private Vector2 Velocity { get; set; } = Vector2.right;
 
 	private void Awake()
 	{
@@ -18,36 +18,52 @@ public class BallBase : MonoBehaviour, IBall
 		InitializeBounds();
 	}
 
+	protected void InitializeVelocity()
+	{
+		rb.velocity = Velocity;
+	}
 
-	[field: SerializeField]
-	public float Speed { get; set; } = 5f;
+
+	[field: SerializeField] public float Speed { get; set; } = 5f;
 
 	public Bounds BallBounds { get; set; } = new Bounds(
 		new Vector3(0, 0),
 		new Vector3(19.75f, 11f));
+
+	protected void UpdateVelocity()
+	{
+		rb.velocity = Velocity.normalized * Speed;
+	}
 
 	public virtual void Move()
 	{
 		var spriteBounds = SpriteRenderer.bounds;
 
 		var pos = rb.position;
-		var velocity = Velocity;
-		var v = velocity * (Speed * Time.fixedDeltaTime);
 
-		var newV = CheckCollisions(velocity, pos + v);
+		var newV = CheckCollisions(pos);
 
-		var newVelocity = newV * (Speed * Time.fixedDeltaTime);
-		
-		pos.y = Mathf.Clamp(pos.y + newVelocity.y,
+		pos.y = Mathf.Clamp(pos.y,
 			BallBounds.min.y + spriteBounds.extents.y,
 			BallBounds.max.y - spriteBounds.extents.y);
 
-		pos.x = Mathf.Clamp(pos.x + newVelocity.x,
+		pos.x = Mathf.Clamp(pos.x,
 			BallBounds.min.x + spriteBounds.extents.x,
 			BallBounds.max.x - spriteBounds.extents.y);
 
 		rb.position = pos;
-		Velocity = newV;
+		Velocity    = newV;
+	}
+
+	private void OnCollisionEnter2D(Collision2D other)
+	{
+		var point = other.GetContact(0);
+
+		Debug.Log(point.point);
+
+		var v = Vector2.Reflect(point.point * -1, point.normal);
+
+		Velocity = v;
 	}
 
 	public virtual void DoFixedUpdate()
@@ -58,31 +74,31 @@ public class BallBase : MonoBehaviour, IBall
 	private void InitializeBounds()
 	{
 		const float screenAspect = 16f / 9f;
-		var cameraHeight = cam.orthographicSize * 2;
+		var         cameraHeight = cam.orthographicSize * 2;
 
 		BallBounds = new Bounds(cam.transform.position,
 			new Vector3(cameraHeight * screenAspect, cameraHeight, 0));
 	}
 
-	private Vector2 CheckCollisions(in Vector2 velocity, in Vector2 pos)
+	private Vector2 CheckCollisions(in Vector2 pos)
 	{
-		var v = velocity;
+		var v        = rb.velocity;
 		var deltaPos = (pos - rb.position).normalized;
 
 		Debug.DrawLine(pos, pos + deltaPos * 5, Color.blue, 2f);
 		var spriteBounds = SpriteRenderer.bounds;
 
 		// Right
-		if (pos.x + spriteBounds.extents.x >= BallBounds.max.x) v = Vector2.Reflect(deltaPos, Vector2.left);
+		if (pos.x - spriteBounds.size.x >= BallBounds.max.x) v = Vector2.Reflect(deltaPos, Vector2.left);
 
 		// Left
-		if (pos.x - spriteBounds.extents.x <= BallBounds.min.x) v = Vector2.Reflect(deltaPos, Vector2.right);
+		if (pos.x + spriteBounds.size.x <= BallBounds.min.x) v = Vector2.Reflect(deltaPos, Vector2.right);
 
 		// Top
-		if (pos.y + spriteBounds.extents.y >= BallBounds.max.y) v = Vector2.Reflect(deltaPos, Vector2.down);
+		if (pos.y + spriteBounds.size.y >= BallBounds.max.y) v = Vector2.Reflect(deltaPos, Vector2.down);
 
 		// Bottom
-		if (pos.y - spriteBounds.extents.y <= BallBounds.min.y) v = Vector2.Reflect(deltaPos, Vector2.up);
+		if (pos.y + spriteBounds.size.y <= BallBounds.min.y) v = Vector2.Reflect(deltaPos, Vector2.up);
 
 		return v.normalized;
 	}
